b49cf4eefc2cc7c9715670b4426df06d6ab2e72e
karavdv <>
2025-02-24 23:45:07 +0100

    Update ThemeToggle.jsx

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./../css/ThemeToggle.css";
+import "./../css/themeToggle.css";
 
 export const ThemeToggle = () => {
   // Haal de eerdere voorkeur uit localStorage of volg het systeemthema
